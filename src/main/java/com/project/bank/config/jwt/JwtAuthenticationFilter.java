0b1b4afdb4f5a04964f39b75a155b7d8ba4e93dc
dongjun <edj0707@naver.com>
2025-06-02 17:10:11 +0900

    feat: 로그인 실패시 로직 처리

--- src/main/java/com/project/bank/config/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/project/bank/config/jwt/JwtAuthenticationFilter.java
@@ -51,9 +51,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             Authentication authentication = authenticationManager.authenticate(authenticationToken);
             return authentication;
         } catch (Exception e) {
-            // authenticationEntryPoint에 걸린다.
+            // unsuccessfulAuthentication 호출
             throw new InternalAuthenticationServiceException(e.getMessage());
         }
+    }
+
+    // 로그인 실패
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                              AuthenticationException failed) throws IOException, ServletException {
+        CustomResponseUtil.unAuthentication(response, "로그인실패");
     }
 
     // return authentication 잘 작동하면 successfulAuthentication 메서드 호출됩니다.
